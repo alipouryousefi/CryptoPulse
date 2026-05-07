a93d5d2b65cfe259383427c6dc4074d824cdf5ec
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2026-05-07 11:11:25 +0330

    implement coin overview and trending coins ui and functionality using the fetcher

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns:[
       {
         protocol:"https",
-        hostname:"assets.coingecko.com"
+        hostname:"coin-images.coingecko.com"
       }
     ]
   }
