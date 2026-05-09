4390248cb14fb0a169e058b9e5d33f165663f684
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2026-05-09 14:49:11 +0330

    add candle stick chart

--- type.d.ts
+++ type.d.ts
@@ -11,10 +11,8 @@ interface CandlestickChartProps {
   coinId: string;
   height?: number;
   children?: React.ReactNode;
-  mode?: 'historical' | 'live';
+  mode?: "historical" | "live";
   initialPeriod?: Period;
-  liveInterval: '1s' | '1m';
-  setLiveInterval: (interval: '1s' | '1m') => void;
 }
 
 interface ConverterProps {
@@ -36,7 +34,14 @@ interface Ticker {
   trade_url: string;
 }
 
-type Period = 'daily' | 'weekly' | 'monthly' | '3months' | '6months' | 'yearly' | 'max';
+type Period =
+  | "daily"
+  | "weekly"
+  | "monthly"
+  | "3months"
+  | "6months"
+  | "yearly"
+  | "max";
 
 interface CoinMarketData {
   id: string;
@@ -250,7 +255,7 @@ interface Category {
 interface UseCoinGeckoWebSocketProps {
   coinId: string;
   poolId: string;
-  liveInterval?: '1s' | '1m';
+  liveInterval?: "1s" | "1m";
 }
 
 interface UseCoinGeckoWebSocketReturn {
@@ -279,12 +284,12 @@ interface DataTableProps<T> {
   bodyCellClassName?: string;
 }
 
-type ButtonSize = 'default' | 'sm' | 'lg' | 'icon' | 'icon-sm' | 'icon-lg';
+type ButtonSize = "default" | "sm" | "lg" | "icon" | "icon-sm" | "icon-lg";
 
 type PaginationLinkProps = {
   isActive?: boolean;
   size?: ButtonSize;
-} & React.ComponentProps<'a'>;
+} & React.ComponentProps<"a">;
 
 interface Pagination {
   currentPage: number;
@@ -296,7 +301,7 @@ interface HeaderProps {
   trendingCoins: TrendingCoin[];
 }
 
-type SearchItemCoin = SearchCoin | TrendingCoin['item'];
+type SearchItemCoin = SearchCoin | TrendingCoin["item"];
 
 interface SearchItemProps {
   coin: SearchItemCoin;
